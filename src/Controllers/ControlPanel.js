65038a442751e3d0687a25875f6aed4592793e52
Zorby <58777319+Zorby-dev@users.noreply.github.com>
2022-01-06 15:30:30 +0100

    revert

--- src/Controllers/ControlPanel.js
+++ src/Controllers/ControlPanel.js
@@ -213,12 +213,15 @@ class ControlPanel {
             Hyperparams.foodProdProb = $('#food-prod-prob').val();
         }.bind(this));
 <<<<<<< HEAD
+<<<<<<< HEAD
         $('#vert-ext-coef').change(function() {
             Hyperparams.vertExtCoef = $('#vert-ext-coef').val();
         }.bind(this));
         $('#gravity').change(function() {
             Hyperparams.gravity = $('#gravity').val();
         }.bind(this));
+=======
+>>>>>>> parent of a2955f1 (lightness)
 =======
 >>>>>>> parent of a2955f1 (lightness)
         $('#lifespan-multiplier').change(function() {
@@ -313,8 +316,11 @@ class ControlPanel {
     updateHyperparamUIValues(){
         $('#food-prod-prob').val(Hyperparams.foodProdProb);
 <<<<<<< HEAD
+<<<<<<< HEAD
         $('#vert-ext-coef').val(Hyperparams.vertExtCoef);
         $('#gravity').val(Hyperparams.gravity);
+=======
+>>>>>>> parent of a2955f1 (lightness)
 =======
 >>>>>>> parent of a2955f1 (lightness)
         $('#lifespan-multiplier').val(Hyperparams.lifespanMultiplier);
--- src/Hyperparameters.js
+++ src/Hyperparameters.js
@@ -5,8 +5,11 @@ const Hyperparams = {
         this.lifespanMultiplier = 100;
         this.foodProdProb = 5;
 <<<<<<< HEAD
+<<<<<<< HEAD
         this.vertExtCoef = 0;
         this.gravity = 0;
+=======
+>>>>>>> parent of a2955f1 (lightness)
 =======
 >>>>>>> parent of a2955f1 (lightness)
         this.killableNeighbors = Neighbors.adjacent;
